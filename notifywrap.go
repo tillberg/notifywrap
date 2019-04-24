a74a77debfe4f6b46bfee19481668ff252a273d0
Dan Tillberg <dan@tillberg.us>
2019-04-24 17:40:38 -0600

    Add workaround kludge to detect file events "in" FUSE bindfs mounts in OSX

--- notifywrap.go
+++ notifywrap.go
@@ -1,12 +1,18 @@
 package notifywrap
 
 import (
+	"bufio"
+	"bytes"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/rjeczalik/notify"
+	"github.com/tillberg/alog"
 )
 
 type Opts struct {
@@ -20,17 +26,17 @@ type Opts struct {
 func WatchRecursive(path string, opts Opts) (<-chan *EventInfo, error) {
 	pathEvents := make(chan *EventInfo, 100)
 	rawPathEvents := make(chan notify.EventInfo, 1000)
-	err := notify.Watch(filepath.Join(path, "..."), rawPathEvents, notify.All)
-	if err != nil {
-		return nil, err
-	}
 	go run(opts, rawPathEvents, pathEvents)
-	if opts.NotifyDirectoriesOnStartup || opts.NotifyFilesOnStartup {
-		rootPath := path
-		if !strings.HasSuffix(rootPath, "/") {
-			rootPath += "/"
+	go walkDirTree(path, opts, pathEvents)
+	err := notify.Watch(filepath.Clean(path)+"/...", rawPathEvents, notify.All)
+	if err != nil {
+		return nil, errors.Wrapf(err, "adding root watch for %q", filepath.Clean(path)+"/...")
+	}
+	if runtime.GOOS == "darwin" {
+		err := handleDarwinBindfsMounts(path, opts, rawPathEvents)
+		if err != nil {
+			return nil, errors.Wrap(err, "adding bindfs-workaround watches")
 		}
-		go walkDirTree(rootPath, pathEvents, opts.NotifyFilesOnStartup)
 	}
 	return pathEvents, nil
 }
@@ -90,23 +96,105 @@ func run(opts Opts, rawPathEvents chan notify.EventInfo, pathEvents chan *EventI
 	}
 }
 
-func walkDirTree(path string, pathEvents chan<- *EventInfo, notifyFiles bool) {
-	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
-		if notifyFiles || info.IsDir() {
+func walkDirTree(rootPath string, opts Opts, pathEvents chan<- *EventInfo) error {
+	return filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
+		path = filepath.Clean(path)
+		var shouldNotify bool
+		if info.IsDir() {
+			shouldNotify = opts.NotifyDirectoriesOnStartup
+		} else {
+			shouldNotify = opts.NotifyFilesOnStartup
+		}
+		if shouldNotify {
 			pathEvents <- &EventInfo{
 				Event: notify.Write,
 				Path:  path,
 			}
-			return nil
 		}
 		return nil
 	})
-	if err != nil {
-		panic(err)
-	}
 }
 
 type EventInfo struct {
 	Event notify.Event
 	Path  string
+}
+
+func handleDarwinBindfsMounts(rootPath string, opts Opts, rawPathEvents chan notify.EventInfo) error {
+	cmd := exec.Command("mount")
+	buf, err := cmd.CombinedOutput()
+	if err != nil {
+		return errors.Wrap(err, "getting list of mounts")
+	}
+	s := bufio.NewScanner(bytes.NewBuffer(buf))
+	for s.Scan() {
+		line := s.Text()
+		parts := strings.SplitN(line, " ", 4)
+		if len(parts) < 4 {
+			continue
+		}
+		src, _, dest, rest := parts[0], parts[1], parts[2], parts[3]
+		if !strings.Contains(rest, "osxfuse") {
+			continue
+		}
+		_, err = os.Stat(src)
+		if err != nil {
+			continue
+		}
+		_, err = os.Stat(dest)
+		if err != nil {
+			continue
+		}
+		alog.Printf("Found apparent bindfs mount at %q. Adding path-rewriting watcher for it at %q.\n", dest, src)
+		err = watchWithPathRewrite(dest, src, opts, rawPathEvents)
+		if err != nil {
+			return errors.Wrapf(err, "adding watch for bindfs mount with source root %q", src)
+		}
+	}
+	err = s.Err()
+	if err != nil {
+		return errors.Wrap(err, "parsing list of mounts")
+	}
+	return nil
+}
+
+type rawEventInfo struct {
+	event notify.Event
+	path  string
+	sys   interface{}
+}
+
+func (i rawEventInfo) Event() notify.Event {
+	return i.event
+}
+
+func (i rawEventInfo) Path() string {
+	return i.path
+}
+
+func (i rawEventInfo) Sys() interface{} {
+	return i.sys
+}
+
+func watchWithPathRewrite(logicalRoot string, actualRoot string, opts Opts, rawPathEvents chan notify.EventInfo) error {
+	origRawPathEvents := make(chan notify.EventInfo, 1000)
+	go func() {
+		for ev := range origRawPathEvents {
+			origPath := ev.Path()
+			logicalPath := strings.Replace(origPath, actualRoot, logicalRoot, 1)
+			if origPath == logicalPath {
+				alog.Printf("@(error:Failed to rewrite path %q in notifywrap.)", origPath)
+			}
+			rawPathEvents <- rawEventInfo{
+				event: ev.Event(),
+				path:  logicalPath,
+				sys:   ev.Sys(),
+			}
+		}
+	}()
+	err := notify.Watch(filepath.Clean(actualRoot)+"/...", origRawPathEvents, notify.All)
+	if err != nil {
+		return errors.Wrapf(err, "adding bindfs-mount-workaround watch for %q", filepath.Clean(actualRoot)+"/...")
+	}
+	return nil
 }
